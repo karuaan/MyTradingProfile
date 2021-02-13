431905f8c1de6ad3b4e28caf7310675c98e82652
Karan Shah <karan@Karans-MBP.fios-router.home>
2021-02-12 21:08:04 -0500

    Initial tetsing completed

--- Portfolio.py
+++ Portfolio.py
@@ -1,6 +1,20 @@
+
 import finnhub
 import datetime
-from yahoo_earnings_calendar import YahooEarningsCalendar
-from Static_Data/keys.py import finnhub_api_key as fh_key
+import yahoo_earnings_calendar
+import pandas
+from keys import finnhub_api_key
 
-finnhub_client = finnhub.Client(api_key="fh_key")
+# Setup Imports
+finnhub_client = finnhub.Client(api_key=finnhub_api_key)
+yec = yahoo_earnings_calendar.YahooEarningsCalendar()
+
+res = finnhub_client.quote('ABNB')
+
+airbnbNextEarningsUnix = int(yec.get_next_earnings_date('ABNB'))
+
+airbnbNextEarningDate = datetime.datetime.utcfromtimestamp(airbnbNextEarningsUnix).strftime('%m-%d-%Y')
+
+print(res)
+print(airbnbNextEarningsUnix)
+print(airbnbNextEarningDate)
